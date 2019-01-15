8745bd82df143477647478b7036210e5e909b184
Sergey Miller <s.melnykov.451@gmail.com>
2019-01-15 15:43:39 +0200

    add card number directive and angular animation

--- src/app5 Directive (card number)/app.module.ts
+++ src/app5 Directive (card number)/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardDirective } from './credit-card-directive/credit-card.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CreditCardDirective,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     AppRoutingModule,
   ],
