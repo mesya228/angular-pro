ab8573d6611d829c73325c980e9463ecc2b20c5a
Sergey Miller <s.melnykov.451@gmail.com>
2019-03-15 16:50:50 +0200

    work at store #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component} from '@angular/core';
+import { Store } from './store';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,18 @@ export class AppComponent {
 
   counter: number = 0;
 
-  constructor() {
-
+  constructor(private store: Store) {
+    this.store.set('todos', [
+      {id: 1},
+      {id: 2},
+      {id: 3}
+    ]);
+    this.store.set('todos2', [
+      {id: 1},
+      {id: 2},
+      {id: 3}
+    ]);
+    console.log(store);
   }
   
 }
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,5 +1,5 @@
-import { BehaviorSubject } from 'rxjs';
-import { distinctUntilChanged } from 'rxjs/operators';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { pluck } from 'rxjs/operators';
 
 import { State } from './state';
 
@@ -10,4 +10,22 @@ const state: State = {
 export class Store {
     private subject = new BehaviorSubject<State>(state);
     private store = this.subject.asObservable();
+
+    get value() {
+        return this.subject.value;
+    }
+
+    select<T>(name: string): Observable<T> {
+        return this.store
+            .pipe(
+                pluck(name)
+            );
+    }
+
+    set(name: string, state: any) {
+        this.subject.next({
+            ...this.value,
+            [name]: state,
+        });
+    }
 }
