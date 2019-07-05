5a94190ed95c335afe343c0721c0a3f27ba78e4d
Sergey Miller <s.melnykov.451@gmail.com>
2019-07-05 17:26:58 +0300

    work at ngrx translating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component} from '@angular/core';
 
-import { Store } from 'store';
+import { TranslateService } from '@ngx-translate/core';
+
+import * as M from 'node_modules/materialize-css'
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,27 @@ import { Store } from 'store';
 })
 export class AppComponent {
 
-  constructor(private store: Store) {
+  activeLanguage: string;
+
+  constructor(
+    private translateService: TranslateService
+  ) {
+    this.translateService.setDefaultLang('en');
+    this.translateService.use(localStorage.getItem('language') || 'en');
+    this.translateService.onLangChange.subscribe(res => {
+      localStorage.setItem('language', res.lang);
+      this.activeLanguage = res.lang == 'en' ? 'English' : res.lang == 'ru' ? 'Русский' : res.lang == 'uk' ? 'Украинский' : '';
+    });
+
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('.dropdown-trigger');
+      var instances = M.Dropdown.init(elems);
+    });
     
+  }
+
+  changeLang(language: string) {
+    this.translateService.use(language);
   }
   
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-import { Store } from './store';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   providers: [
-    Store
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) => { return new TranslateHttpLoader(http) },
+        deps: [HttpClient]
+      }
+    })
   ],
   bootstrap: [AppComponent]
 })
